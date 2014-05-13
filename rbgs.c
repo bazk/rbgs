e58b1046a48970b554878c6164ba578790991e6d
Eduardo L. Buratti <elb09@c3sl.ufpr.br>
2014-05-13 07:05:37 -0300

    Add Gauss-Seidel method
    Signed-off-by: Eduardo L. Buratti <elb09@c3sl.ufpr.br>

--- rbgs.c
+++ rbgs.c
@@ -11,18 +11,16 @@
 
 #define K 2 * M_PI
 
-typedef enum { JACOBI, GAUSS } method_t;
-
+int nx, ny, num_threads, num_iterations;
 double *current_grid, *previous_grid;
+double hx, hy;
 
 double f(double x, double y) {
     return 4 * M_PI * M_PI * sin(2 * M_PI * x) * sinh(2 * M_PI * y);
 }
 
-void borders(int nx, int ny) {
+void init_grid() {
     int ix, iy;
-
-    double hx = 2 / (double) nx;
 
     for (ix=0; ix<=nx; ++ix) {
         current_grid[ix*(ny+1) + 0] = previous_grid[ix*(ny+1) + 0] = 0;
@@ -33,15 +31,18 @@ void borders(int nx, int ny) {
         current_grid[0*(ny+1) + iy] = previous_grid[0*(ny+1) + iy] = 0;
         current_grid[nx*(ny+1) + iy] = previous_grid[nx*(ny+1) + iy] = 0;
     }
+
+    for (ix=1; ix<nx; ++ix) {
+        for (iy=1; iy<ny; ++iy) {
+            current_grid[ix*(ny+1) + iy] = previous_grid[ix*(ny+1) + iy] = 0;
+        }
+    }
 }
 
-void jacobi(int nx, int ny, int num_threads, int num_iterations) {
+void jacobi() {
     int it, ix, iy;
     double *tmp;
     double sum;
-
-    double hx = 2 / (double) nx;
-    double hy = 1 / (double) ny;
 
     for (it=0; it<num_iterations; ++it) {
         tmp = previous_grid;
@@ -52,22 +53,47 @@ void jacobi(int nx, int ny, int num_threads, int num_iterations) {
             for (iy=1; iy<ny; ++iy) {
                 sum = 0;
 
-                sum += (-1 / (hx*hx)) * previous_grid[(ix-1)*(ny+1) + iy];
-                sum += (-1 / (hx*hx)) * previous_grid[(ix+1)*(ny+1) + iy];
-                sum += (-1 / (hy*hy)) * previous_grid[ix*(ny+1) + (iy-1)];
-                sum += (-1 / (hy*hy)) * previous_grid[ix*(ny+1) + (iy+1)];
+                sum -= previous_grid[(ix-1)*(ny+1) + iy] / (hx*hx);
+                sum -= previous_grid[(ix+1)*(ny+1) + iy] / (hx*hx);
+                sum -= previous_grid[ix*(ny+1) + (iy-1)] / (hy*hy);
+                sum -= previous_grid[ix*(ny+1) + (iy+1)] / (hy*hy);
 
                 current_grid[ix*(ny+1) + iy] = (f(ix*hx, iy*hy) - sum) /
-                                                    (2 / hx*hx + 2 / hy*hy + K*K);
+                                                    (2 / (hx*hx) + 2 / (hy*hy) + (K*K));
             }
         }
     }
 }
 
+void gauss() {
+    int it, ix, iy;
+    double sum;
+
+    for (it=0; it<num_iterations; ++it) {
+        for (ix=1; ix<nx; ++ix) {
+            for (iy=1; iy<ny; ++iy) {
+                sum = 0;
+
+                sum -= current_grid[(ix-1)*(ny+1) + iy] / (hx*hx);
+                sum -= current_grid[(ix+1)*(ny+1) + iy] / (hx*hx);
+                sum -= current_grid[ix*(ny+1) + (iy-1)] / (hy*hy);
+                sum -= current_grid[ix*(ny+1) + (iy+1)] / (hy*hy);
+
+                current_grid[ix*(ny+1) + iy] = (f(ix*hx, iy*hy) - sum) /
+                                                    (2 / (hx*hx) + 2 / (hy*hy) + (K*K));
+            }
+        }
+    }
+}
+
+void cleanup() {
+    free(current_grid);
+    free(previous_grid);
+}
+
 int main(int argc, char **argv) {
-    int ix, iy, nx, ny, num_threads, num_iterations;
-    double hx, hy;
-    method_t method;
+    int ix, iy;
+    double sum, residue;
     FILE *solution;
 
     if (argc != 6) {
@@ -79,17 +105,8 @@ int main(int argc, char **argv) {
     ny = atoi(argv[2]);
     num_threads = atoi(argv[3]);
     num_iterations = atoi(argv[4]);
-
-    if (strcmp(argv[5], "j") == 0) {
-        method = JACOBI;
-    }
-    else if (strcmp(argv[5], "g") == 0) {
-        method = GAUSS;
-    }
-    else {
-        fprintf(stderr, "error: '%s' is not a valid calculation method (valid options are 'j' for Jacobi or 'g' for Gauss)\n", argv[5]);
-        exit(1);
-    }
+    hx = 2 / (double) nx;
+    hy = 1 / (double) ny;
 
     if ((nx < 1) || (ny < 1)) {
         fprintf(stderr, "error: %dx%d are not valid dimensions for discretization\n", nx, ny);
@@ -106,15 +123,6 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
-    hx = 2 / (double) nx;
-    hy = 1 / (double) ny;
-
-    // open solution.txt file to write the resulting solution
-    if ((solution = fopen("solution.txt", "w")) == NULL) {
-        fprintf(stderr, "error: failed to open 'solution.txt' for writing\n");
-        exit(1);
-    }
-
     // allocate grids
     current_grid = (double*) malloc((nx+1) * (ny+1) * sizeof(double));
     previous_grid = (double*) malloc((nx+1) * (ny+1) * sizeof(double));
@@ -123,29 +131,53 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
-    // initialize grids
-    for (ix=0; ix<=nx; ++ix) {
-        for (iy=0; iy<=ny; ++iy) {
-            current_grid[ix*(ny+1) + iy] = previous_grid[ix*(ny+1) + iy] = 0;
-        }
+    init_grid();
+
+    // calculate using the appropriate method
+    switch (argv[5][0]) {
+    case 'j':
+        jacobi();
+        break;
+    case 'g':
+        gauss();
+        break;
+    default:
+        fprintf(stderr, "error: '%s' is not a valid calculation method (valid options are 'j' for Jacobi or 'g' for Gauss-Seidel)\n", argv[5]);
+        cleanup();
+        exit(1);
     }
 
-    // initialize borders
-    borders(nx, ny);
-
-    // calculate
-    jacobi(nx, ny, num_threads, num_iterations);
-
     // write solution to file
+    if ((solution = fopen("solution.txt", "w")) == NULL) {
+        fprintf(stderr, "error: failed to open 'solution.txt' for writing\n");
+        cleanup();
+        exit(1);
+    }
     for (ix=0; ix<=nx; ++ix) {
         for (iy=0; iy<=ny; ++iy) {
             fprintf(solution, "%lf %lf %lf\n", ix*hx, iy*hy, current_grid[ix*(ny+1) + iy]);
         }
         fprintf(solution, "\n");
     }
-
-    free(current_grid);
-    free(previous_grid);
     fclose(solution);
+
+    // calculate l2-norm of the residue
+    residue = 0;
+    for (ix=1; ix<nx; ++ix) {
+        for (iy=1; iy<ny; ++iy) {
+            sum = 0;
+
+            sum -= current_grid[(ix-1)*(ny+1) + iy] / (hx*hx);
+            sum -= current_grid[(ix+1)*(ny+1) + iy] / (hx*hx);
+            sum += current_grid[ix*(ny+1) + iy] * (2 / (hx*hx) + 2 / (hy*hy) + (K*K));
+            sum -= current_grid[ix*(ny+1) + (iy-1)] / (hy*hy);
+            sum -= current_grid[ix*(ny+1) + (iy+1)] / (hy*hy);
+
+            residue += pow(f(ix*hx, iy*hy) - sum, 2);
+        }
+    }
+    printf("%lf\n", sqrt(residue));
+
+    cleanup();    
     exit(0);
 }
